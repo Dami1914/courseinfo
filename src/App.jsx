033ad652365de8d591ca3429596130372208d7b1
Damilola Jibowu <Aesthetico.design@gmail.com>
2023-09-11 16:33:25 +0100

    part2 2.1 courseinfo made alot of changes to my component; i stupidly difined components in my app compoenent. and aslo created a new course coponent to house every other component except the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,75 @@
 import { useState } from "react";
 
 function App() {
+  
   const course = {
-    name: "Half Stack application development",
+    id: 1,
+    name: 'Half Stack application development',
     parts: [
-      { name: "Fundamentals of React", exercises: 10 },
-      { name: "Using props to pass data", exercises: 7 },
-      { name: "State of a component", exercises: 14 },
-    ],
-  };
-
-
-  function Header({ course }) {
-    return <h1>{course.name}</h1>;
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
   }
 
-  function Content({ parts }) {
-    function Part({ part }) {
-      return (
-        <p>
-          {part.name} {part.exercises}
-        </p>
-      );
-    }
+  return <Course course={course} />
 
-    return (
-      <div>
-        <Part part={parts[0]} />
-        <Part part={parts[1]} />
-        <Part part={parts[2]} />
-      </div>
-    );
-  }
+}
 
-  function Total({ parts }) {
-    console.log(parts[1].exercises);
-    return (
-      <p>
-        Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-      </p>
-    );
-  }
-
+//Total Component
+function Total({ parts }) {
+  console.log(parts[1].exercises);
   return (
-    <>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
+    <p>
+      Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
   );
 }
 
+// Part Component
+function Part({ part }) {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+}
+
+//Content Component
+function Content({ parts }) {
+  return (
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  );
+}
+
+//Header Component
+function Header({ course }) {
+  return <h1>{course.name}</h1>;
+}
+
+//Course Component
+function Course({course}){
+  return(
+    <>
+      <Header course={course} />
+      <Content parts={course.parts} />
+    </>
+  )
+}
 export default App;
