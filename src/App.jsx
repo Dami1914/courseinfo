5913784cbc98072acbd75aabd58f6490c4b0ae2e
Damilola Jibowu <Aesthetico.design@gmail.com>
2023-09-12 14:48:58 +0100

    part2 2.4 courseinfo added nodejs course and also subcourses to the previous one

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
 import { useState } from "react";
 
 function App() {
+
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
   
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+ 
   return ( 
-    <Course course={course}/>
+    <>
+      <Course course={courses[0]}/>
+      <Course course={courses[1]}/>
+    </>
+    
   )
 }
 
@@ -51,9 +75,10 @@ function Total({parts}) {
 
 //Part Component
 function Part({ part }) {
+  console.log(part)
   return (
     <p>
-      {part.name} {part.exercises}
+      {part?.name} {part?.exercises}
     </p>
   );
 }
